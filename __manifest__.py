05b4f3dce033d83ed9cf82d18b5acb141fc5d6ef
Andrei05231 <carare.andrei57@gmail.com>
2025-12-09 10:20:57 +0000

    initial commit (default crm view with commented fields)

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Custom CRM Lead",
+    "version": "0.1",
+    "depends": ['base','crm'],
+    "category": "Human Resources",
+    "description": "custom fields in CRM lead",
+    "data": [
+        "views/lead_views.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+}
